afca50de309e3bbd856b7da5b4b02f3bdd5be7ea
Maris Orbidans <maris.orbidans@gmail.com>
2011-08-30 11:27:52 +0100

    nil's removed

--- src/sudoku/core.clj
+++ src/sudoku/core.clj
@@ -23,15 +23,13 @@
 (defn smallest-change-set [vek]
   (let [sets (for [i (range (count vek)) :when (zero? (vek i))]
                [i (difference (set (range 1 10)) (select-row vek i) (select-column vek i) (select-square vek i))])]
-    (if (not (seq sets))
-      nil
+    (if (seq sets)
       (reduce #(if (< (count (%1 1)) (count (%2 1))) %1 %2) sets))))
 
 (defn solve [vek]
   (let [s (smallest-change-set vek)]
     (if (nil? s)
       vek
-      (if (not (seq (s 1)))
-        nil
-        (let [sol (for [i (s 1)] (solve (assoc vek (first s) i))) ]
+      (if (seq (s 1))
+         (let [sol (for [i (s 1)] (solve (assoc vek (first s) i))) ]
           (find-first #(not (nil? %)) sol))))))
