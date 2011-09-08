797facbbd8c0e40c8b8e8f0a301fe25185d680b3
Maris Orbidans <Maris.Orbidans@gmail.com>
2011-09-08 01:10:57 +0100

    agents

--- src/sudoku/main.clj
+++ src/sudoku/main.clj
@@ -3,13 +3,29 @@
   (:use sudoku.io)
   (:gen-class))
 
+
+(defn parmap [f coll] (let [agents (map #(agent %) (into '() coll))]
+                        ( (println f)
+                          
+                         #_(doseq [a agents]
+                             (send a f))
+                         
+                         (println (deref (nth agents 1)))
+
+                         (map
+                          #(deref %)
+                          agents))))
+
+(def functions {:s map :p pmap :a parmap})
+
 (defn -main [& args]
 
   (cond (= (count args) 1) (let [board (read-file (first args))
                                  solution (solve board)]
                              (write-file solution))
         (= (count args) 2) (let [boards (read-batch-file (first args))
-                                 solutions (pmap #(solve %) boards)]
+                                 funct (functions (keyword (nth args 1)))
+                                 solutions (funct #(solve %) boards)]
                              (doseq [solution solutions] (write-file solution))
                              (shutdown-agents))
         :else (println "Usage: SudokuSolver <file name>")))
