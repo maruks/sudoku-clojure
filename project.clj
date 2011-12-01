23172c1fa457977ce1b1a27e66c7d424ec02dab0
maris <maris@maris-ThinkPad-T61.(none)>
2011-12-01 11:20:02 +0000

    swank dep update

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Sudoku solver"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[swank-clojure "1.3.1"]
+  :dev-dependencies [[swank-clojure "1.3.3"]
                      [midje "1.1.1"]]
   :aot [#"sudoku.*"]
   :main sudoku.main)
