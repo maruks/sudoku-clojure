ecd5b627eeb368b034557f3bd9e637fe51609026
Maris Orbidans <maris.orbidans@gmail.com>
2011-12-15 01:28:32 +0000

    made code more readable

--- src/sudoku/core.clj
+++ src/sudoku/core.clj
@@ -2,33 +2,36 @@
   (:use clojure.set)
   (:use [clojure.contrib.seq :only [find-first]]))
 
-(defn select-row [vek i]
-  (let [start (- i (rem i 9))
+(defn select-row [board index]
+  (let [start (- index (rem index 9))
         end (+ start 9)]
-    (set (for [e (range start end)](vek e)))))
+    (set (map #(board %) (range start end)))))
 
-(defn select-column [vek i]
-  (let [start (rem i 9)
+(defn select-column [board index]
+  (let [start (rem index 9)
         end (+ start 73)]
-    (set (for [e (range start end 9)](vek e)))))  
+    (set (map #(board %) (range start end 9)))))
 
-(defn select-square [vek i]
-  (let [x (- (rem i 9)(rem (rem i 9) 3))
-        y (- (quot i 9) (rem (quot i 9) 3))
+(defn select-square [board index]
+  (let [x (- (rem index 9)(rem (rem index 9) 3))
+        y (- (quot index 9) (rem (quot index 9) 3))
         spos (+ x (* y 9))
         r (range spos (+ spos 3))
         rng (union (set r) (set (map #(+ % 9) r)) (set (map #(+ % 18) r)))]
-    (set (for [e rng](vek e)))))
+    (set (map #(board %) rng))))
 
-(defn smallest-change-set [vek]
-  (let [sets (for [i (range (count vek)) :when (zero? (vek i))]
-               [i (difference (set (range 1 10)) (select-row vek i) (select-column vek i) (select-square vek i))])]
-    (if (seq sets)
-      (reduce #(if (< (count (second %1)) (count (second %2))) %1 %2) sets))))
+(defn possible-values [board index]
+  (difference (set (range 1 10)) (select-row board index) (select-column board index) (select-square board index)))
 
-(defn solve [vek]
-  (if-let [s (smallest-change-set vek)]
-    (when-let [x (second s)]
-      (let [sol (for [i (sort x)] (solve (assoc vek (first s) i))) ]
-        (find-first #(not (nil? %)) sol)))
-    vek))
+(defn smallest-change-set [board]
+  (when-let [sets (not-empty (map #(vector % (possible-values board %))
+                                  (filter #(zero? (board %)) (range (count board)))))]
+    (reduce #(if (< (count (second %1)) (count (second %2))) %1 %2) sets)))
+
+(defn solve [board]
+  (if-let [change (smallest-change-set board)]
+    (let [index (first change)
+          numbers (second change)
+          solutions (map #(solve (assoc board index %)) (sort numbers))]
+      (find-first #(not (nil? %)) solutions))
+    board))
